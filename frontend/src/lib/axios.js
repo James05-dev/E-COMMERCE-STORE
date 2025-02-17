ff52c3d55a5f4e6b77ae4bd2311185462d4a4b04
Atoyebi James Anuoluwapo <anuoluwapojames48@gmail.com>
2025-02-17 14:08:16 +0100

    Update axios.js
    url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.mode === "development"
-      ? "https://e-commerce-store-kdiw.onrender.com"
+      ? "https://e-commerce-store-kdiw.onrender.com/api"
       : "/api",
   withCredentials: true,
 });
