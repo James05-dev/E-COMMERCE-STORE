448642c7443a25658afedeb4c85293962d7c2cba
Atoyebi James Anuoluwapo <anuoluwapojames48@gmail.com>
2025-02-17 13:34:27 +0100

    Update axios.js
    main updated backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.mode === "development"
-      ? "https://backend-e-commerce-navy.vercel.app/api"
+      ? "https://e-commerce-store-kdiw.onrender.com/api"
       : "/api",
   withCredentials: true,
 });
