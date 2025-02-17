df708cb67efe977b2383cc12dd8dac8012232d52
Atoyebi James Anuoluwapo <anuoluwapojames48@gmail.com>
2025-02-17 13:10:44 +0100

    Update axios.js
    changed the url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.mode === "development"
-      ? "http://localhost:5000/api"
+      ? "https://backend-e-commerce-navy.vercel.app/api"
       : "/api",
   withCredentials: true,
 });
