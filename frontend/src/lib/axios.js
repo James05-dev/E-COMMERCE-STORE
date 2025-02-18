fa5c1f38f78b60288899509bfd2e53fead88dd05
Atoyebi James <anuoluwapojames48@gmail.com>
2025-02-18 10:42:01 +0100

    pushed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.mode === "development"
-      ? "https://e-commerce-store-kdiw.onrender.com/api"
+      ? "http://localhost:5000/api"
       : "https://e-commerce-store-kdiw.onrender.com/api",
   withCredentials: true,
 });
