51d9600f6a4f7aa20de48ab895b50f38c8c51fb5
Atoyebi James <anuoluwapojames48@gmail.com>
2025-02-18 10:17:23 +0100

    allow mutiple orign CORS

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import router from "./routes/auth.route.js";
@@ -17,10 +17,24 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
-app.use(cors({ 
-  origin: "https://e-commerce-store-lake-gamma.vercel.app/",  // Set a specific origin, not "*"
-  credentials: true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://e-commerce-store-lake-gamma.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      } 
+    },
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -29,9 +43,9 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
-app.get("/", (req, res)=>{
-  res.send("tested")
-})
+app.get("/", (req, res) => {
+  res.send("tested");
+});
 
 app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
