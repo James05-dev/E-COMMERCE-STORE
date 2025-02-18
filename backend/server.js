414576c80b52bfc8a695acf1bcbfa931e416749b
Atoyebi James <anuoluwapojames48@gmail.com>
2025-02-18 10:47:21 +0100

    committed

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "10mb" }));
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://e-commerce-store-lake-gamma.vercel.app",
+  "https://e-commerce-store-kdiw.onrender.com",
+  // "https://e-commerce-store-lake-gamma.vercel.app",
 ];
 
 app.use(
