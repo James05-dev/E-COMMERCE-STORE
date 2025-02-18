20ce859b066fa06ca9a626273268f40c7e6b14c9
Atoyebi James <anuoluwapojames48@gmail.com>
2025-02-18 10:05:03 +0100

    Add CORS support and update axios base URL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.mode === "development"
-      ? "https://localhost:5000/api"
-      : "/api",
+      ? "https://e-commerce-store-kdiw.onrender.com/api"
+      : "https://e-commerce-store-kdiw.onrender.com/api",
   withCredentials: true,
 });
 export default axiosInstance;
