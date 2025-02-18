f1495fea04986e757b17a3c80531fe2397465a23
Atoyebi James <anuoluwapojames48@gmail.com>
2025-02-18 13:37:44 +0100

    vb

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,13 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      } 
+    },
     credentials: true,
   })
 );
