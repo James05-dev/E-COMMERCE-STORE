b40c8e0996667ed005a69f7512468cd847e4dc61
Atoyebi James <anuoluwapojames48@gmail.com>
2025-02-19 12:00:04 +0100

    Update .gitignore to include .env.development and refactor axios base URL configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.mode === "development"
-      ? "http://localhost:5000/api"
-      : "https://e-commerce-store-kdiw.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
 export default axiosInstance;
